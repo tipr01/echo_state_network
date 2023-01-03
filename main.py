bdae933264365a247afa343a220bc6a66c310732
tipr01 <75940228+tipr01@users.noreply.github.com>
2023-01-03 15:53:38 +0100

    Update main.py
    new edit 6

--- main.py
+++ main.py
@@ -3,25 +3,28 @@ from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
 
 #reservoir size
-n = 300
+n = 200
 
 #timedomain
-tmax = 100
+tmax = 40
 
 #values of numbers of washout-, training -and prediction phase
-washout = 500
-training = 100000
-prediction_time = 10 #time unit
+washout = 50
+training = 1000
+prediction_time = 5 #time unit
 
 #leaking rate
 l = 0.9
+
+lmd = 10
+density = 0.1
 
 
 def act(x):
     return np.tanh(x)
 
 #function that creates a random adjacency matrix with spectral radius less than 1
-def random_adjacency_matrix(n):
+def random_adjacency_matrix(n, p):
     # matrix = np.random.choice([-1, 0, 1], size=(n, n), p=[0, 0.9, 0.1])
     matrix = np.random.uniform(-1, 1, size=(n, n))
 
@@ -31,7 +34,7 @@ def random_adjacency_matrix(n):
 
     for i in range(n):
         for j in range(n):
-            if np.random.random() < 0.9:
+            if np.random.random() < 1 - p:
                 matrix[j][i] = 0
             matrix[i][j] = matrix[j][i]
 
@@ -46,9 +49,9 @@ def random_adjacency_matrix(n):
     #         else:
     #             matrix[i][j] = matrix[j][i]
 
-    matrix = np.reshape(matrix, (n,n))
+    # matrix = np.reshape(matrix, (n,n))
     # set spectral radius to a constant near 1, less than 1 ensures the echo state property
-    matrix = (1.0 / np.max(np.linalg.eigvals(matrix))) * matrix
+    matrix = (lmd / np.max(np.real(np.linalg.eigvals(matrix)))) * matrix
     return matrix
 
 
@@ -79,28 +82,29 @@ sol = solve_ivp(lorenz, (0, tmax), x0, method='RK45', t_eval=t, args=(a, b, c))
 
 #plot of the lorenz attractor
 time = np.array(sol.t)
-coo = np.array(sol.y)
-x, y, z = coo[0], coo[1], coo[2]
+data = np.array(sol.y)
+x, y, z = data
 
-x, y, z = np.array(x), np.array(y), np.array(z)
 
-data = np.array([x, y, z])
+# x, y, z = np.array(x), np.array(y), np.array(z)
+
+#data = np.array([x, y, z])
 print(np.shape(data[:, 0]))
 
 #extend solution
-xnew = np.array([x[-1], y[-1], z[-1]])
+xnew = data[:, -1]
 
 t = np.linspace(0, prediction_time, prediction)
 sol = solve_ivp(lorenz, (0, prediction_time), xnew, method='RK45', t_eval=t, args=(a, b, c))
 
 time = np.array(sol.t)
 coo = np.array(sol.y)
-x, y, z = coo[0], coo[1], coo[2]
 
 #
-# plt.plot(t, x, color='r', label='x')
-# plt.plot(t, y, color='g', label='y')
-# plt.plot(t, z, color='b', label='z')
+#plt.plot(data.T, color='r', label='x')
+# plt.plot(t, coo[0], color='r', label='x')
+# plt.plot(t, coo[1], color='g', label='y')
+# plt.plot(t, coo[2], color='b', label='z')
 #
 # plt.show()
 
@@ -109,7 +113,8 @@ plt.figure('lorenz solution')
 ax = plt.axes(projection='3d')
 
 # Data for a three-dimensional line
-ax.plot3D(x, y, z, 'blue')
+# ax.plot3D(*data, 'blue')
+ax.plot3D(*coo, 'blue')
 
 
 #implementation of the echo state network
@@ -119,7 +124,7 @@ r = np.zeros(n)
 
 #random (input-) matrix that maps the data for every timestep into the reservoir
 #Win = (np.random.rand(n, 3) - 0.5) * 2
-Win = np.random.uniform(-1, 1, size=(n, 3))
+Win = np.random.uniform(-0.1, 0.1, size=(n, 3))
 #Win = np.array([np.identity(3) for i in range(n // 3)]).reshape((n, 3))
 
 
@@ -129,7 +134,7 @@ for i in range(n):
             Win[i][j] = 0
 
 #random adjacency matrix which connects some of the "reservoir points"
-W_r = random_adjacency_matrix(n)
+W_r = random_adjacency_matrix(n, density)
 
 
 for k in range(washout):
@@ -139,12 +144,11 @@ for k in range(washout):
 
 #creates matrix of reservoir states in training phase
 X = []
-R = []
 print('Finding reservoir states...')
 perc = 0
 
 for k in range(washout, steps):
-    if (k * 100) // training  > perc:
+    if ((k - washout) * 100) // training  > perc:
         perc += 1
         if perc % 10 == 0:
             print(f"{perc // 10}", sep='', end='', flush=True)
@@ -153,11 +157,10 @@ for k in range(washout, steps):
         if perc == 99:
             print('')
 
-    r = (1 - l) * r + l * act(Win @ data[:, k] + W_r @ r)
     X.append(r)
+    r = (1 - l) * r + l * act(Win @ data[:, k] + W_r @ r)
 
-    if k in range(steps-3, steps):
-        R.append(r)
+
 
 print('Completed.')
 # plt.figure()
@@ -171,16 +174,25 @@ X = np.array(X).T
 #matrix of solutions in training phase
 Xtarget = data[:, washout:steps]
 
+print(np.shape(Xtarget))
 
 #computation of the output matrix via ridge regression with regularization term beta
-beta = 1e-8
-Wout = Xtarget @ X.T @ np.linalg.inv(X @ X.T + beta * np.identity(n))
+# beta = 1e-12
+# Wout = Xtarget @ X.T @ np.linalg.inv(X @ X.T + beta * np.identity(n))
 
+#print(np.linalg.norm(Wout @ X - Xtarget))
+
+# Xtarget = Wout @ X
+Wout = np.linalg.lstsq(X.T, Xtarget.T, rcond=None)[0]
+Wout = Wout.T
+
+print(np.linalg.norm(Wout @ X - Xtarget))
+#
 # #computation of the error
 # s = 0
 # r = np.zeros(n)
 # for k in range(washout):
-#     r = (1 - l) * r + l * np.arctan(Win @ data[:, k] + W_r @ r)
+#     r = (1 - l) * r + l * act(Win @ data[:, k] + W_r @ r)
 #     s = s + np.linalg.norm(data[:, k] - (Wout @ r), None) ** 2
 # print(s)
 #
@@ -192,6 +204,8 @@ Wout = Xtarget @ X.T @ np.linalg.inv(X @ X.T + beta * np.identity(n))
 
 
 print('generating prediction...')
+
+R = []
 perc = 0
 for k in range(prediction):
     if (k * 100) // prediction  > perc:
@@ -203,9 +217,12 @@ for k in range(prediction):
         if perc == 99:
             print('')
 
-    r = (1 - l) * R[-1] + l * act(Win @ (Wout @ R[-3]) + W_r @ R[-1])
-    #print(Wout @ r)
+    # r = (1 - l) * r + l * act(Win @ data[:, k] + W_r @ r)
+    print(data[:, k], Wout @ r)
     R.append(r)
+    r = (1 - l) * r + l * act(Win @ (Wout @ r) + W_r @ r)
+    #print(Wout @ r)
+
 
 print('end prediction')
 R = np.array(R).T
@@ -215,7 +232,7 @@ X_pred = Wout @ R
 
 # for i in range(prediction):
 #     print(X_pred[i])
-[print(col) for col in X_pred.T]
+#[print(col) for col in X_pred.T]
 
 
 
@@ -228,5 +245,20 @@ xline = X_pred[0]
 yline = X_pred[1]
 zline = X_pred[2]
 ax.plot3D(xline, yline, zline, 'red')
+
+plt.figure('coordinates')
+
+plt.plot(t, coo[0], color='r', label='x')
+plt.plot(t, coo[1], color='g', label='y')
+plt.plot(t, coo[2], color='b', label='z')
+
+plt.plot(t, xline, 'r--', label='x_pred', )
+plt.plot(t, yline, 'g--', label='y_pred')
+plt.plot(t, zline, 'b--', label='z_pred')
+
+plt.legend()
+
+plt.show()
+
 
 plt.show()
