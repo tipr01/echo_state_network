3bbeaae65cc46cb7e502b63aab840a416103f728
tipr01 <75940228+tipr01@users.noreply.github.com>
2023-01-04 19:04:18 +0100

    Create space.py

---
+++ venv/Include/space.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+
+def move_figure(position="top-right"):
+    '''
+    Move and resize a window to a set of standard positions on the screen.
+    Possible positions are:
+    top, bottom, left, right, top-left, top-right, bottom-left, bottom-right
+    '''
+
+    mgr = plt.get_current_fig_manager()
+    mgr.full_screen_toggle()  # primitive but works to get screen size
+    py = mgr.canvas.height()
+    px = mgr.canvas.width()
+
+    d = 10  # width of the window border in pixels
+    if position == "top":
+        # x-top-left-corner, y-top-left-corner, x-width, y-width (in pixels)
+        mgr.window.setGeometry(d, 4*d, px - 2*d, py/2 - 4*d)
+    elif position == "bottom":
+        mgr.window.setGeometry(d, py/2 + 5*d, px - 2*d, py/2 - 4*d)
+    elif position == "left":
+        mgr.window.setGeometry(d, 4*d, px/2 - 2*d, py - 4*d)
+    elif position == "right":
+        mgr.window.setGeometry(px/2 + d, 4*d, px/2 - 2*d, py - 4*d)
+    elif position == "top-left":
+        mgr.window.setGeometry(d, 4*d, px/2 - 2*d, py/2 - 4*d)
+    elif position == "top-right":
+        mgr.window.setGeometry(px/2 + d, 4*d, px/2 - 2*d, py/2 - 4*d)
+    elif position == "bottom-left":
+        mgr.window.setGeometry(d, py/2 + 5*d, px/2 - 2*d, py/2 - 4*d)
+    elif position == "bottom-right":
+        mgr.window.setGeometry(px/2 + d, py/2 + 5*d, px/2 - 2*d, py/2 - 4*d)
+
